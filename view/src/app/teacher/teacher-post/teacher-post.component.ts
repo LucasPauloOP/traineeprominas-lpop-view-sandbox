082023ae34719edca8f49b71e6630bcd2b4284f9
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2019-06-19 12:29:38 -0300

    teacher post

--- view/src/app/teacher/teacher-post/teacher-post.component.ts
+++ view/src/app/teacher/teacher-post/teacher-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import {Service} from '../../service.service';
 
 @Component({
   selector: 'app-teacher-post',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherPostComponent implements OnInit {
 
-  constructor() { }
+  teacher: FormGroup;
+  name = '';
+  lastname = '';
+  phd = false;
+  result = false;
+  constructor(private router: Router, private service: Service, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.teacher = this.formBuilder.group({
+      name: [null, Validators.required],
+      lastname: [null, Validators.required],
+      phd: [null, Validators.required]
+    });
+  }
+
+  postTeacher(form: NgForm){
+    this.result = true;
+    this.service.postUser(form).subscribe(res => {
+      this.result = false;
+      this.router.navigate(['/teacher']);
+    }, (err) =>{
+      console.log(err);
+      this.result = false;
+    });
   }
 
 }
